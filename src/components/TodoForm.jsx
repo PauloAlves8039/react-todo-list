bd0a472fab113ed6389638e26c5fbc88a96116dd
PauloAlves8039 <pj38alves@gmail.com>
2024-11-29 20:23:28 -0300

    feat: implemented task addition

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (!value || !category) {
+      return;
+    }
+    
+    addTodo(value, category);
+
+    setValue("");
+    setCategory("");
+  }
+  
   return (
     <div className="todo-form">
         <h2>Create Task</h2>
 
-        <form>
-            <input type="text" placeholder="Digite o título" />
-            <select>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter title" value={value} onChange={(e) => setValue(e.target.value)} />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option value="">Select a category</option>
                 <option value="Trabalho">Trabalho</option>
                 <option value="Pessoal">Pessoal</option>
@@ -21,4 +37,4 @@ const TodoForm = () => {
   )
 };
 
-export default TodoForm
+export default TodoForm;
