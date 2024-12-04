131c166020c715cfe93db6ec403d90a9c4be433c
PauloAlves8039 <pj38alves@gmail.com>
2024-12-04 08:45:18 -0300

    feat: added screenshot and data cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,7 @@ import "./App.css";
 
 function App() {
   
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: "criar funcionalidade x no sistema",
-      category: "Trabalho",
-      isCompleted: false,
-    },
-    {
-      id:2,
-      text: "Ir pra academia",
-      category: "Pessoal",
-      isCompleted: false,
-    },
-    {
-      id:3,
-      text: "Estudar React",
-      category: "Estudos",
-      isCompleted: false,
-    }
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("All");
